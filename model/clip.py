b9b5672b8fba6332f1b0f73439b46bd1676b0270
Savarus <furunqiao@163.com>
2024-01-20 23:23:20 +0800

    feat(clip.py) 构造CLIP主结构

--- model/clip.py
+++ model/clip.py
@@ -1,2 +1,13 @@
 import torch
 import torch.nn as nn
+
+
+class CLIP(nn.Module):
+    def __init__(self):
+        super(CLIP, self).__init__()
+        self.image_encoder = ImageEncoder()
+
+    def forward(self, image, text):
+
+        return image_features, text_features
+
